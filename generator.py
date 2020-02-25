8b9e6cbbc77d9bc545bb8573a4de859da4867f32
evlx87 <evilnyagaming@yandex.ru>
2020-02-25 16:31:52 +0300

    fix def in generator.py

--- generator.py
+++ generator.py
@@ -5,24 +5,26 @@ from datetime import datetime
 import pyperclip
 
 
+NEW_PASS = []  # Временно созданная переменная для тестов
+
+
 def generator(request):
     """Функция для генерации пароля"""
     pass_symbols = string.ascii_letters + string.digits + '!@#$%&'
     pass_len = int(request)
-    new_pass = ''.join(random.choice(pass_symbols) for x in range(pass_len))
-
-    return new_pass
+    output_pass = ''.join(random.choice(pass_symbols) for x in range(pass_len))
+    NEW_PASS.append(output_pass)
+    return output_pass
 
 
 def create_file(request):
     """Фунция для сохранения сгенерированного пароля в файл"""
     file = open('pass_dir/password_' +
-                str(datetime.now().strftime('%Y_%m_%d_%H_%M_%S')) +
+                str(datetime.now().strftime('%Y_%m_%d_%H_%M')) +
                 '.txt', 'tw', encoding='utf8')
     input_data = str(request)
     file.write(input_data)
     file_name = str(file)
-
     return file_name
 
 
