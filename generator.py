50548a7d192c3e829c552833a18b5c6ff68ceaca
evlx87 <evilnyagaming@yandex.ru>
2020-02-25 12:18:09 +0300

    pep8 correct

--- generator.py
+++ generator.py
@@ -1,25 +1,26 @@
-import string
+
 import random
+import string
 from datetime import datetime
 
 
-def generator(pass_len):
+def generator(request):
     pass_symbols = string.ascii_letters + string.digits + '!@#$%&'
-    pass_len = int(pass_len)
+    pass_len = int(request)
     new_pass = ''.join(random.choice(pass_symbols) for x in range(pass_len))
 
     return new_pass
 
 
-# def create_file(request):
-#     new_pass_file = open('pass_dir/password_' + str(datetime.now().strftime('%Y_%m_%d_%H_%M_%S')) + '.txt',
-#                          'tw',
-#                          encoding='utf8')
-#     input_data = str(request)
-#     new_pass_file.write(input_data)
-#     file_name = str(new_pass_file)
-#
-#     return file_name
+def create_file(request):
+    file = open('pass_dir/password_' +
+                str(datetime.now().strftime('%Y_%m_%d_%H_%M_%S')) +
+                '.txt', 'tw', encoding='utf8')
+    input_data = str(request)
+    file.write(input_data)
+    file_name = str(file)
+
+    return file_name
 
 
 print(generator(input("Укажите желаемую длину пароля(число): ")))
