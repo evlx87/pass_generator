fdb7b448dd6b3d6dbf788fbc05d4730b64a5c115
evlx87 <evlx@mail.ru>
2023-05-11 22:54:15 +0300

    fixed a problem with copy_btn

--- program.py
+++ program.py
@@ -5,8 +5,9 @@ from generator_funcs import gen_pass, create_file, copy_to_clipboard
 
 class Shell:
     """Графическая оболочка программы генератора паролей"""
-
     def __init__(self, main_window):
+        self.generated_password = ''
+
         frame = Frame(main_window)
         frame.pack()
 
@@ -27,24 +28,29 @@ class Shell:
             frame,
             text="Сгенерировать новый пароль",
             fg='green',
-            command=lambda: output.config(text=gen_pass(spin.get())))
+            command=lambda: (self.update_password(output, gen_pass(spin.get()))))
         generate_btn.pack(fill=BOTH)
 
         """Кнопка для сохранения пароля в файл"""
         save_btn = Button(
             frame,
             text="Сохранить в файл",
-            command=lambda: output.config(text=create_file(output['text'])))
+            command=lambda: (create_file(self.generated_password), self.update_label(output)))
         save_btn.pack(fill=BOTH)
 
         """Кнопка для копирования сгенерированного пароля в буфер обмена"""
         copy_btn = Button(
             frame,
             text="Скопировать пароль",
-            command=lambda: output.config(text=copy_to_clipboard(output['text'])))
+            command=lambda: output.config(text=copy_to_clipboard(self.generated_password)))
         copy_btn.pack(fill=BOTH)
 
-    generated_password = ""
+    def update_password(self, label, password):
+        self.generated_password = password
+        self.update_label(label)
+
+    def update_label(self, label):
+        label.config(text=self.generated_password)
 
 
 WINDOW = Tk()
