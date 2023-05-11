08cdb8330e4888d341a872a8e0f07b9e182c15ac
evlx87 <evlx@mail.ru>
2023-05-11 22:20:08 +0300

    minimized class Shell code

--- program.py
+++ program.py
@@ -7,54 +7,44 @@ class Shell:
     """Графическая оболочка программы генератора паролей"""
 
     def __init__(self, main_window):
-        self.frame = Frame(main_window)
-        self.frame.pack()
+        frame = Frame(main_window)
+        frame.pack()
+
         """Информационный текст в окне программы"""
-        self.label = Label(
-            self.frame,
-            text="Укажите длину пароля и \nвыберите необходимое действие")
-        self.label.pack(side=TOP)
+        label = Label(frame, text="Укажите длину пароля и \nвыберите необходимое действие")
+        label.pack(side=TOP)
+
         """Выбор длины пароля"""
-        self.spin = Spinbox(main_window, from_=0, to=20)
-        self.spin.pack(fill=BOTH)
+        spin = Spinbox(main_window, from_=0, to=20)
+        spin.pack(fill=BOTH)
+
         """Вывод сгенерированного пароля в окне программы"""
-        self.output = Label(self.frame, fg='red', font='30')
-        self.output.pack()
+        output = Label(frame, fg='red', font='30')
+        output.pack()
+
         """Кнопка для генерации пароля"""
-        self.generate_btn = Button(
-            self.frame,
+        generate_btn = Button(
+            frame,
             text="Сгенерировать новый пароль",
             fg='green',
-            command=self.generate)
-        self.generate_btn.pack(fill=BOTH)
+            command=lambda: output.config(text=gen_pass(spin.get())))
+        generate_btn.pack(fill=BOTH)
 
         """Кнопка для сохранения пароля в файл"""
-        self.save_btn = Button(
-            self.frame,
+        save_btn = Button(
+            frame,
             text="Сохранить в файл",
-            command=self.save_file)
-        self.save_btn.pack(fill=BOTH)
+            command=lambda: output.config(text=create_file(output['text'])))
+        save_btn.pack(fill=BOTH)
 
         """Кнопка для копирования сгенерированного пароля в буфер обмена"""
-        self.copy_btn = Button(
-            self.frame,
+        copy_btn = Button(
+            frame,
             text="Скопировать пароль",
-            command=self.copy_clip)
-        self.copy_btn.pack(fill=BOTH)
-        self.generated_password = ""
+            command=lambda: output.config(text=copy_to_clipboard(output['text'])))
+        copy_btn.pack(fill=BOTH)
 
-    def generate(self):
-        """Генерация пароля"""
-        self.generated_password = gen_pass(self.spin.get())
-        self.output.config(text=self.generated_password)
-
-    def save_file(self):
-        """Сохранение пароля в файл"""
-        self.output.config(text=create_file(self.output['text']))
-
-    def copy_clip(self):
-        """Копирование пароля в буфер обмена"""
-        self.output.config(text=copy_to_clipboard(self.output['text']))
+    generated_password = ""
 
 
 WINDOW = Tk()
